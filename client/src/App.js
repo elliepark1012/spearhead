0cc3360c336e6ff2002a65ed1be8f3a416e4993e
Ellie Park <elliepark1012@gmail.com>
2023-05-20 18:21:17 -0400

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [brands, setBrands] = useState([])
+  const [errors, setErrors] = useState(false)
+
+  useEffect(() => {
+    fetch('/brands')
+    .then(res => {
+      if(res.ok){
+        res.json().then(setBrands)
+      } else {
+        res.json().then(data => setErrors(data.error))
+      }
+    })
+  },[])
+
+  const addBrand = (brand) => setBrands(current => [...current, brand])
+
+  const updateBrand = (updatedBrand) => setBrands(current => {
+    return current.map(brand => {
+      if(brand.id === updatedBrand.id) {
+        return updatedBrand 
+      } else {
+        return brand
+      }
+    })
+  })
+
+  const deleteBrand = (id) => setBrands(current => current.filter(p => p.id !== id))
+
+  if(errors) return <h1>{errors}</h1>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path='/brands/new'>
+
+        </Route>
+        <Route path='/brands/:id/edit'>
+          
+        </Route>
+        <Route path='brands/:id'>
+
+        </Route>
+        <Route path='users/:id'>
+
+        </Route>
+        <Route exact path='/'>
+          <Home brands={brands} />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </div>
   );
 }
